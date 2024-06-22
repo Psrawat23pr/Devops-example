223fdccfdefa741e133af3859e7a9a0c06a4ad90
Psrawat23pr <93816710+Psrawat23pr@users.noreply.github.com>
2024-06-22 10:10:14 +0000

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
-public class Welcome {
+public class hello {
     public static void main(String[] args) {
         // Print a welcome message
         System.out.println("Welcome to Java Programming!");
